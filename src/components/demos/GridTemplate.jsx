e5486327f3c07931b33fcad373eb90375654f0b5
Batsave <111178330+Batsave@users.noreply.github.com>
2025-08-04 12:56:40 +0000

    Ajout du composant GridTemplate pour démontrer les colonnes et lignes de la mise en page en grille.

---
+++ src/components/demos/GridTemplate.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Demo.scss";
+
+export default function GridTemplateDemo() {
+  return (
+    <div className="card">
+      <h3>Grid Template Columns & Rows</h3>
+      <div className="container"
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(3, 1fr)",
+          gridTemplateRows: "100px 200px",
+          gap: "1rem"
+        }}
+      >
+        <div className="item">1</div>
+        <div className="item">2</div>
+        <div className="item">3</div>
+        <div className="item">4</div>
+        <div className="item">5</div>
+        <div className="item">6</div>
+      </div>
+
+      <pre>{`.container {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: 100px 200px;
+}`}</pre>
+    </div>
+  );
+}
