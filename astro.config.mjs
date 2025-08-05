8bd68c5bec7f2bad9e27b236023102991fbf0039
Batsave <111178330+Batsave@users.noreply.github.com>
2025-08-05 08:57:04 +0000

    Suppression de l'intégration d'Analytics de Vercel et ajout du script d'insights dans la configuration d'Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import { fileURLToPath } from "url";
 import rehypeCallouts from "rehype-callouts";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-import Analytics from '@vercel/analytics/astro'
 
 export default defineConfig({
   site: "https://dev.batsave.tv",
@@ -18,7 +17,6 @@ export default defineConfig({
   integrations: [
     react(),
     sitemap(),
-    Analytics(),
     starlight({
       title: "DEV Récap",
       description: "Récapitulatif du développement web, HTML, CSS, JavaScript et bonnes pratiques.",
@@ -47,7 +45,13 @@ export default defineConfig({
         { tag: "meta", attrs: { property: "og:url", content: "https://dev.batsave.tv" } },
         { tag: "meta", attrs: { name: "twitter:card", content: "summary_large_image" } },
         { tag: "meta", attrs: { name: "google-site-verification", content: "ANXl8OG3ubA9eai8sI34GhInc6-KmJ-P8SirJEWHE5k" } },
-        
+        {
+      tag: "script",
+      attrs: {
+        defer: true,
+        src: "/_vercel/insights/script.js",
+      },
+    },
       ],
       defaultLocale: "fr",
       locales: {
